d266a9e80bc1f0a0812e916ebafc4d870cd2b316
Z223I <BruceWilsonPH@yahoo.com.ph>
2018-03-29 13:06:07 -0500

    Print the finished list.

--- Refactor/Refactor.py
+++ Refactor/Refactor.py
@@ -1,6 +1,9 @@
 #!usr/bin/python3
 
 import sys
+#
+#
+#
 
 # Ask for file if not on the command line
 if len(sys.argv) < 2:
@@ -14,6 +17,9 @@ with open(filename, "r") as fo:
     lines = fo.readlines()
 
 lines = [x.strip() for x in lines]
+#
+#
+#
 
 length = len(lines)
 
@@ -23,6 +29,25 @@ print(*lines, sep='\n')
 
 # Setup 2 dimensional array
 matches = [[] for i in range(length)]
+#
+#
+#
 
+# Build array.
 for i in range(length):
-    matches[i].append(1)
+    index = -1
+    for j in range(length):
+        index += 1
+        if lines[i] == lines[j] :
+            # if lines i and j match append i at index.
+            matches[index].append(i)
+
+
+# Check the array.
+# for each i, search for consecutive numbers to find chunks of code
+# that have been copied.
+for i in range(length):
+    print('i: ', i)
+    print(*matches[i], sep=' ')
+
+
